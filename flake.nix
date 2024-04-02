494e5ad3e48ab30454ef14d1da74013a3ec4348c
Thirukumaran Vaseeharan <vasyharan@gmail.com>
2024-04-02 02:39:26 +0000

    rust toolchain

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
     unstable.url= "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   nixConfig = {
@@ -10,15 +14,25 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, unstable, flake-utils, ... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system: 
+  outputs = { self, nixpkgs, unstable, flake-utils, fenix, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
       let
+        overlays = [ fenix.overlays.default ];
         pkgs = import nixpkgs {
-          inherit system;
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.fenix.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-sH6tcgUqft0TlybCuF4LirxmePW09mZDgu/SFP1bxGE=";
         };
       in {
       devShells.default = pkgs.mkShell {
-        buildInputs = [];
+        buildInputs = [
+          rustToolchain
+        ];
+        packages = with pkgs; [
+          cargo-watch
+        ];
       };
     });
 }
